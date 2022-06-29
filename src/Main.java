a6039e6e91dfab1f01d442d77b369ef44d20576e
Bair Arshaev <ars-bair@yandex.ru>
2022-06-29 17:26:55 +0300

    Edit code 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
 import java.util.Random;
+import java.util.concurrent.RecursiveTask;
 
 public class Main {
 
     final static int ARRAY_CAPACITY = 10;
+    final static int R = 2;
 
     public static void main(String[] args) {
-       // int[] array = generateArray(ARRAY_CAPACITY);
-        System.out.println(array.length);
-        System.out.println(sum(array));
+        int[] array = generateArray(ARRAY_CAPACITY);
+        System.out.println("Массив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Однопоточный результат...");
+        System.out.println("Сумма элементов массива: " + sum(array));
+        System.out.println("Среднее арифметическое значение массива: " + sum(array)/ARRAY_CAPACITY);
+
+        System.out.println("Многопоточный результат...");
+        ArraySumTask arraySumTask = new ArraySumTask(0, ARRAY_CAPACITY, array);
+        System.out.println("Сумма элементов массива: " + arraySumTask.compute());
+        System.out.println("Среднее арифметическое значение массива: " + arraySumTask.compute()/ARRAY_CAPACITY);
     }
 
     static int[] generateArray(int length) {
         Random random = new Random();
         int[] array = new int[length];
         for (int i = 0; i < length; i++) {
-            array[i] = random.nextInt(150);      }
-        return new int[length];
+            array[i] = random.nextInt(100);
+        }
+        return array;
     }
 
     static int sum(int[] array) {

1c9a6e52e8356dc4bb179074188b77403c4c9431
Bair Arshaev <ars-bair@yandex.ru>
2022-06-30 15:36:32 +0300

    Edit code 3

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,35 @@ import java.util.concurrent.RecursiveTask;
 
 public class Main {
 
-    final static int ARRAY_CAPACITY = 10;
-    final static int R = 2;
+    final static int ARRAY_CAPACITY = 10000;
+    final static int R = 100;
 
     public static void main(String[] args) {
+
         int[] array = generateArray(ARRAY_CAPACITY);
-        System.out.println("Массив:");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
+
+        long start1 = System.currentTimeMillis();
 
         System.out.println("Однопоточный результат...");
         System.out.println("Сумма элементов массива: " + sum(array));
         System.out.println("Среднее арифметическое значение массива: " + sum(array)/ARRAY_CAPACITY);
 
+        long finish1 = System.currentTimeMillis();
+        long elapsed1 = finish1 - start1;
+        System.out.println("Прошло времени, мс: " + elapsed1);
+
+        System.out.println();
+
+        long start2 = System.currentTimeMillis();
+
         System.out.println("Многопоточный результат...");
         ArraySumTask arraySumTask = new ArraySumTask(0, ARRAY_CAPACITY, array);
         System.out.println("Сумма элементов массива: " + arraySumTask.compute());
         System.out.println("Среднее арифметическое значение массива: " + arraySumTask.compute()/ARRAY_CAPACITY);
+
+        long finish2 = System.currentTimeMillis();
+        long elapsed2 = finish2 - start2;
+        System.out.println("Прошло времени, мс: " + elapsed2);
     }
 
     static int[] generateArray(int length) {
@@ -36,6 +46,14 @@ public class Main {
     static int sum(int[] array) {
         int s = 0;
         for (int i = 0; i < array.length; i++) {
+            s = s + array[i];
+        }
+        return s;
+    }
+
+    static int subSum(int[] array, int start, int end) {
+        int s = 0;
+        for (int i = start; i < end; i++) {
             s = s + array[i];
         }
         return s;
